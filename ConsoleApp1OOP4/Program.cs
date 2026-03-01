4d8174819ebdfcf0915b697204dc8b60840ebfe6
Koko3000onsy <onsykoko79@gmail.com>
2026-03-01 14:02:18 +0200

    Add project files.

---
+++ ConsoleApp1OOP4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1OOP4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region Q1
+            // static binding :Static binding means the method call is resolved at compile time.
+            // it does in compile time when have method overriding , method hiding , static and private method.
+            //class Animal
+            //{
+            //public static void Speak()
+            //{
+            //    Console.WriteLine("Animal sound");
+            //}
+            //dynamic binding:Dynamic binding means the method call is resolved at runtime based on the actual object.
+            //it does in runtime when have virtual method,overriding method , interfacecalls
+            //class Animal
+            //{
+            //public virtual void Speak()
+            //{
+            //    Console.WriteLine("Animal sound");
+            //}
+            //}
+
+            //class Dog : Animal
+            //{
+            //public override void Speak()
+            //{
+            //    Console.WriteLine("Dog barks");
+            //}
+            //}
+
+            #endregion
+            #region Q2
+            //Method 
+
+            #endregion
+        }
+    }
+}
